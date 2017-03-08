c1d26818ff00c95a127568a9850504d10a743ad7
Angel Newton <xanewton@gmail.com>
2017-03-08 13:29:16 -0600

    Add verb details

--- app/src/main/java/com/xengar/android/englishverbs/ui/DetailsActivity.java
+++ app/src/main/java/com/xengar/android/englishverbs/ui/DetailsActivity.java
@@ -15,13 +15,21 @@
  */
 package com.xengar.android.englishverbs.ui;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.xengar.android.englishverbs.R;
+import com.xengar.android.englishverbs.data.VerbContract;
+import com.xengar.android.englishverbs.data.VerbContract.VerbEntry;
 
 import static com.xengar.android.englishverbs.utils.Constants.VERB_ID;
 import static com.xengar.android.englishverbs.utils.Constants.VERB_NAME;
@@ -29,8 +37,10 @@ import static com.xengar.android.englishverbs.utils.Constants.VERB_NAME;
 /**
  * DetailsActivity
  */
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
 
+    private static final int EXISTING_VERB_LOADER = 0;
     private FloatingActionButton fabAdd, fabDel;
     private long verbID;
 
@@ -47,6 +57,9 @@ public class DetailsActivity extends AppCompatActivity {
         String title = bundle.getString(VERB_NAME);
         getSupportActionBar().setTitle(title);
         showFavoriteButtons();
+
+        // Initialize a loader to read the verb data from the database and display it
+        getLoaderManager().initLoader(EXISTING_VERB_LOADER, null, this);
     }
 
     /**
@@ -67,6 +80,80 @@ public class DetailsActivity extends AppCompatActivity {
         /*
         if (cursor != null)
             cursor.close();*/
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                VerbEntry._ID,
+                VerbEntry.COLUMN_INFINITIVE,
+                VerbEntry.COLUMN_SIMPLE_PAST,
+                VerbEntry.COLUMN_PAST_PARTICIPLE,
+                VerbEntry.COLUMN_REGULAR,
+                VerbEntry.COLUMN_DEFINITION,
+                VerbEntry.COLUMN_PHONETIC_INFINITIVE,
+                VerbEntry.COLUMN_PHONETIC_SIMPLE_PAST,
+                VerbEntry.COLUMN_PHONETIC_PAST_PARTICIPLE,
+                VerbEntry.COLUMN_SAMPLE_1,
+                VerbEntry.COLUMN_SAMPLE_2,
+                VerbEntry.COLUMN_SAMPLE_3 };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                // Query the content URI for the verb id
+                ContentUris.withAppendedId(VerbContract.VerbEntry.CONTENT_URI, verbID),
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+
+        // Proceed with moving to the first row of the cursor and reading data from it
+        // (This should be the only row in the cursor)
+        if (cursor.moveToFirst()) {
+            TextView infinitive = (TextView) findViewById(R.id.infinitive);
+            TextView simplePast = (TextView) findViewById(R.id.simple_past);
+            TextView pastParticiple = (TextView) findViewById(R.id.past_participle);
+            TextView pInfinitive = (TextView) findViewById(R.id.phonetic_infinitive);
+            TextView pSimplePast = (TextView) findViewById(R.id.phonetic_simple_past);
+            TextView pPastParticiple = (TextView) findViewById(R.id.phonetic_past_participle);
+            TextView definition = (TextView) findViewById(R.id.definition);
+            TextView sample1 = (TextView) findViewById(R.id.sample1);
+            TextView sample2 = (TextView) findViewById(R.id.sample2);
+            TextView sample3 = (TextView) findViewById(R.id.sample3);
+
+            // Update the views on the screen with the values from the database
+            infinitive.setText(
+                    cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_INFINITIVE)));
+            simplePast.setText(
+                    cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_SIMPLE_PAST)));
+            pastParticiple.setText(
+                    cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_PAST_PARTICIPLE)));
+            pInfinitive.setText(
+                    cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_PHONETIC_INFINITIVE)));
+            pSimplePast.setText(
+                    cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_PHONETIC_SIMPLE_PAST)));
+            pPastParticiple.setText(
+                    cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_PHONETIC_PAST_PARTICIPLE)));
+
+            definition.setText(cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_DEFINITION)));
+            sample1.setText(cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_SAMPLE_1)));
+            sample2.setText(cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_SAMPLE_2)));
+            sample3.setText(cursor.getString(cursor.getColumnIndex(VerbEntry.COLUMN_SAMPLE_3)));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 
 }
